adfa2985a8baf8d83c3766d63978f269dc9d464c
Rhys <rhys@rhysjenkins.uk>
2023-05-29 09:51:07 +0100

    Added random image gen

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from 'react'
 import tokyo from '/assets/tokyo-jezael-melgoza-unsplash.jpg'
+import london from '/assets/london-anthony-delanoix-unsplash.jpg'
+import santiago from '/assets/santiago-juan-pablo-ahumada-unsplash.jpg'
+import paris from '/assets/paris-john-towner-unsplash.jpg'
+import hongKong from '/assets/hong-kong-chapman-chow-unsplash.jpg' 
 
 function Image() {
+    const [imageSrc, setImageSrc] = useState(null)
+
+    useEffect(() => {
+        const getRandomImage = () => {
+            const images = [tokyo, london, santiago, paris, hongKong]
+            const randomNum = Math.floor(Math.random() * 5)
+            setImageSrc(images[randomNum])
+        }
+
+        getRandomImage()
+    }, [])
+
     return (
         <>
-            <img className='image' src={tokyo} alt="City background image"/>
+            <img className='image' src={imageSrc} alt="City background image"/>
             <div className='overlay'></div>
         </>
     )
